cdf13f2558649b0152fb26cd6a525dc8d947edd8
Ashir Ali <muhammadashirali@icloud.com>
2021-07-17 23:58:13 -0700

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  Calendar things
+
   var _clientID = new ClientId(Secret.getId(), "");
   const _scopes = const [cal.CalendarApi.calendarScope];
 
