a4a2856c50a30a7bb636940bcda8106a7497afe6
Rishi-k-s <rishikrishna.sr@gmail.com>
2021-07-20 17:09:24 +0530

    list of activities

---
+++ lib/test_code/liveclass_tester.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ViewVirtualClassList extends StatefulWidget {
+  @override
+  _ViewVirtualClassListState createState() => _ViewVirtualClassListState();
+}
+
+class _ViewVirtualClassListState extends State<ViewVirtualClassList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: EdgeInsets.only(
+            top: 8.0,
+          ),
+          child: Column(
+            children: [],
+          ),
+        ));
+  }
+}
